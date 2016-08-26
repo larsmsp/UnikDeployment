7c95680e19413e10234ddaf06a0b41aa34cf0715
Lars Martin S. Pedersen <larsmsp@gmail.com>
2016-08-26 14:42:02 +0200

    Improved deployment script. Better support for Windows

--- fabfile.py
+++ fabfile.py
@@ -5,12 +5,16 @@ from fabric.api import *
 from fabric.contrib.files import exists
 
 tool.init_config()
-repo_url = tool.config.get(tool.CONFIG_SECTION, tool.REPO_URL_OPTION)
-repo_name = tool.config.get(tool.CONFIG_SECTION, tool.REPO_NAME_OPTION)
-raspberry_ip = tool.config.get(tool.CONFIG_SECTION, tool.RASPBERRY_IP_OPTION)
+git_path = tool.config.get(tool.GIT_CONFIG_SECTION, tool.GIT_PATH_OPTION)
+repo_url = tool.config.get(tool.REPO_CONFIG_SECTION, tool.REPO_URL_OPTION)
+repo_name = tool.config.get(tool.REPO_CONFIG_SECTION, tool.REPO_NAME_OPTION)
+raspberry_ip = tool.config.get(tool.RASPBERRY_CONFIG_SECTION, tool.RASPBERRY_IP_OPTION)
+
 logging.info('Repository URL: %s', repo_url)
 logging.info('Repository name: %s', repo_name)
 logging.info('Raspberry Pi: %s', raspberry_ip)
+
+
 
 env.hosts = [raspberry_ip]
 env.user = 'pi'
@@ -26,11 +30,12 @@ def clone():
 
 
 def commit():
-    local('git add -p && git commit')
+    local('%s add -p' % git_path)
+    local('%s commit' % git_path)
 
 
 def push():
-    local('git push')
+    local('%s push' % git_path)
 
 
 def prepare_deploy():
@@ -43,7 +48,8 @@ def pull():
 
 
 def deploy():
-    directory = 'home/pi/%s' % repo_name
+    prepare_deploy()
+    directory = '/home/pi/%s' % repo_name
     if exists(directory):
         with cd(directory):
             pull()
--- tool.py
+++ tool.py
@@ -5,17 +5,29 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 CONFIG_FILE = 'deploy.cfg'
-CONFIG_SECTION = 'unik'
-RASPBERRY_IP_OPTION = 'raspberry_ip'
-REPO_URL_OPTION = 'repository_url'
-REPO_NAME_OPTION = 'repository_name'
+
+GIT_CONFIG_SECTION = 'git'
+GIT_PATH_OPTION = 'path'
+
+RASPBERRY_CONFIG_SECTION = 'raspberry'
+RASPBERRY_IP_OPTION = 'ip'
+
+REPO_CONFIG_SECTION = 'repository'
+REPO_URL_OPTION = 'url'
+REPO_NAME_OPTION = 'name'
 
 config = SafeConfigParser()
 
+
+def prompt_git():
+    path = raw_input('Please input the path of your git executable:')
+    config.set(GIT_CONFIG_SECTION, GIT_PATH_OPTION, path)
+
+
 def prompt_repository():
     url = raw_input('Please input git url for your hub project:')
-    config.set(CONFIG_SECTION, REPO_URL_OPTION, url)
-    config.set(CONFIG_SECTION, REPO_NAME_OPTION, _get_repository_name(url))
+    config.set(REPO_CONFIG_SECTION, REPO_URL_OPTION, url)
+    config.set(REPO_CONFIG_SECTION, REPO_NAME_OPTION, _get_repository_name(url))
 
 
 def _get_repository_name(url):
@@ -24,20 +36,24 @@ def _get_repository_name(url):
 
 def prompt_raspberry_ip():
     ip = raw_input('Please input the ip of you raspberry pi:')
-    config.set(CONFIG_SECTION, RASPBERRY_IP_OPTION, ip)
+    config.set(RASPBERRY_CONFIG_SECTION, RASPBERRY_IP_OPTION, ip)
 
 
 def init_config():
     if not isfile(CONFIG_FILE):
-        config.add_section(CONFIG_SECTION)
+        config.add_section(GIT_CONFIG_SECTION)
+        config.add_section(REPO_CONFIG_SECTION)
+        config.add_section(RASPBERRY_CONFIG_SECTION)
         prompt_repository()
         prompt_raspberry_ip()
     else:
         with open(CONFIG_FILE) as config_fp:
             config.readfp(config_fp)
-    if not config.has_option(CONFIG_SECTION, REPO_URL_OPTION):
+    if not config.has_option(GIT_CONFIG_SECTION, GIT_PATH_OPTION):
+        prompt_git()
+    if not config.has_option(REPO_CONFIG_SECTION, REPO_URL_OPTION):
         prompt_repository()
-    if not config.has_option(CONFIG_SECTION, RASPBERRY_IP_OPTION):
+    if not config.has_option(RASPBERRY_CONFIG_SECTION, RASPBERRY_IP_OPTION):
         prompt_raspberry_ip()
     with open(CONFIG_FILE, 'w') as config_fp:
         config.write(config_fp)
